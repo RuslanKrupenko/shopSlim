4f32da0d19a81942dd498a32236b5ab10192cbad
Ruslan <krupenkorv@gmail.com>
2017-10-10 23:34:14 +0300

    Add CategoryController and ProductController for category and product pages. Add static

--- config/settings.php
+++ config/settings.php
@@ -1,5 +1,8 @@
 <?php
 
+$template = "default";
+$templateAdmin = "admin";
+
 return ["settings" => [
     "displayErrorDetails" => true,
     "addContentLengthHeader" => false,
@@ -8,6 +11,13 @@ return ["settings" => [
         "user" => "root",
         "pass" => "12345",
         "dbname" => "shop",
+    ],
+    "smarty" => [
+        "tpl_prefix" => __DIR__ . "/../src/views/{$template}/",
+        "tpl_admin_prefix" => __DIR__ . "/src/views/{$templateAdmin}/",
+        "tpl_postfix" => ".tpl",
+        "tpl_web_path" => "/templates/{$template}/",
+        "tpl_admin_web_path" => "/templates/{$templateAdmin}/"
     ]
 ]
 ];
